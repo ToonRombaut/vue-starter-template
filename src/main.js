1d74196a1f5edc902a10065631ab05de1ac5e7f1
Toon Rombaut <toon.rombaut@hotmail.com>
2022-01-12 18:39:28 +0100

    added sanitize.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from "@router";
 import store from "@store";
 
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
+
 new Vue({
     router,
     store,
